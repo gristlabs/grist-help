5ed6a3b1d0662cbeef01c0107899cfabb293ee5b
Dmitry S <dsagal+git@gmail.com>
2019-10-27 02:56:33 -0400

    Style API to hide left-column, show right one, include links into main help menu, and adjust spacing

--- api/build.sh
+++ api/build.sh
@@ -14,7 +14,10 @@ fi
 
 if [[ "$1" = "" ]]; then
   set -x
-  $cli bundle api/grist.yml --output=help/api-docs.html --options.hideDownloadButton
+  $cli bundle api/grist.yml -t api/template.hbs --output=help/api-docs.html --options.theme.spacing.sectionVertical=2 \
+    --options.hideDownloadButton --disableGoogleFont --options.pathInMiddlePanel \
+    --options.theme.breakpoints.medium=50rem --options.theme.breakpoints.large=50rem \
+    --options.theme.menu.width=0px
 else
   $cli "$@"
 fi
