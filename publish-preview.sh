920c83b03bf598c8a6f867bd5539e03141703005
Dmitry S <dsagal+git@gmail.com>
2019-11-21 16:20:54 -0500

    Allow the main documentation site to be indexed, but not the preview site

--- publish-preview.sh
+++ publish-preview.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo 'support-preview.getgrist.com' > help/CNAME
+
+# Don't let the preview site get crawled and indexed.
+cat > help/robots.txt <<EOF
+User-agent: *
+Disallow: /
+EOF
+
 ./env/bin/mkdocs gh-deploy -r preview
 git checkout -- help/CNAME
 echo "Published to https://$(git show preview/gh-pages:CNAME)"
