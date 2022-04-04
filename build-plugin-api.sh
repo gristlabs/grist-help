da65b352efb9bbb6fb1172bbab9969f792ad2d90
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2022-04-04 16:02:39 -0400

    move typedoc to grist-help; reorder link

--- build-plugin-api.sh
+++ build-plugin-api.sh
@@ -5,14 +5,17 @@ set -e
 if [[ "$1" = "" ]]; then
   echo "Usage: $0 grist-checkout-dir"
   echo "  Rebuilds help/plugin-api"
+  echo "Before running, make sure packages are up to date, and Grist has been built."
   exit 2
 fi
 
 DIR=$PWD
 cd $1
-npx typedoc --out $DIR/tmp-code --plugin "typedoc-plugin-markdown" \
-    --hideBreadcrumbs \
-    --logLevel Verbose
+$DIR/node_modules/.bin/typedoc \
+  --out $DIR/tmp-code\
+  --plugin "typedoc-plugin-markdown" \
+  --hideBreadcrumbs \
+  --logLevel Verbose
 rm -rf $DIR/help/code/interfaces $DIR/help/code/modules
 mv $DIR/tmp-code/interfaces $DIR/help/code/interfaces
 mv $DIR/tmp-code/modules $DIR/help/code/modules
