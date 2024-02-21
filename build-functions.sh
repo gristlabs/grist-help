81f1ffef024c24851a0b3fa80dd6a17b3a8452a9
Florent FAYOLLE <florent.fayolle@beta.gouv.fr>
2024-02-21 11:03:27 +0100

    Adapt README and scripts to new structure

--- build-functions.sh
+++ build-functions.sh
@@ -4,9 +4,9 @@ set -e
 
 if [[ "$1" = "" ]]; then
   echo "Usage: $0 grist-checkout-dir"
-  echo "  Rebuilds help/functions.md from doc-comments of Grist functions"
+  echo "  Rebuilds help/en/docs/functions.md from doc-comments of Grist functions"
   exit 2
 fi
 
 root=$1
-"$root"/sandbox_venv3/bin/python ./mkpydocs.py "$root" 'grist,functions' help/functions.md
+"$root"/sandbox_venv3/bin/python ./mkpydocs.py "$root" 'grist,functions' help/en/docs/functions.md
