50c038adedf3d28b96b0ecee8d60eac03e026cec
Florent FAYOLLE <florent.fayolle@beta.gouv.fr>
2024-02-22 09:03:59 +0100

    Clean up docs.py

--- docs.py
+++ docs.py
@@ -42,13 +42,6 @@ site_path = Path("site").absolute()
 build_site_path = Path("site_build").absolute()
 
 
-@lru_cache
-def is_mkdocs_insiders() -> bool:
-    return False
-    # version = metadata.version("mkdocs-material")
-    # return "insiders" in version
-
-
 def get_base_config_path() -> Dict[str, Any]:
     return mkdocs.utils.yaml_load(base_config_path.read_text(encoding="utf-8"))
 
@@ -69,14 +62,6 @@ def complete_existing_lang(incomplete: str):
     for lang_path in get_lang_paths():
         if lang_path.is_dir() and lang_path.name.startswith(incomplete):
             yield lang_path.name
-
-
-@app.callback()
-def callback() -> None:
-    if is_mkdocs_insiders():
-        os.environ["INSIDERS_FILE"] = "../en/mkdocs.insiders.yml"
-    # For MacOS with insiders and Cairo
-    os.environ["DYLD_FALLBACK_LIBRARY_PATH"] = "/opt/homebrew/lib"
 
 
 @app.command()
@@ -113,8 +98,6 @@ def build_lang(
     """
     insiders_env_file = os.environ.get("INSIDERS_FILE")
     print(f"Insiders file {insiders_env_file}")
-    if is_mkdocs_insiders():
-        print("Using insiders")
     lang_path: Path = docs_path / lang
     if not lang_path.is_dir():
         typer.echo(f"The language translation doesn't seem to exist yet: {lang}")
@@ -138,70 +121,6 @@ def build_lang(
     shutil.copytree(build_site_dist_path, dist_path, dirs_exist_ok=True)
     os.chdir(current_dir)
     typer.secho(f"Successfully built docs for: {lang}", color=typer.colors.GREEN)
-
-
-index_sponsors_template = """
-{% if sponsors %}
-{% for sponsor in sponsors.gold -%}
-<a href="{{ sponsor.url }}" target="_blank" title="{{ sponsor.title }}"><img src="{{ sponsor.img }}"></a>
-{% endfor -%}
-{%- for sponsor in sponsors.silver -%}
-<a href="{{ sponsor.url }}" target="_blank" title="{{ sponsor.title }}"><img src="{{ sponsor.img }}"></a>
-{% endfor %}
-{% endif %}
-"""
-
-
-def generate_readme_content() -> str:
-    en_index = en_docs_path / docs_folder_name / "index.md"
-    content = en_index.read_text("utf-8")
-    match_pre = re.search(r"</style>\n\n", content)
-    match_start = re.search(r"<!-- sponsors -->", content)
-    match_end = re.search(r"<!-- /sponsors -->", content)
-    sponsors_data_path = en_docs_path / "data" / "sponsors.yml"
-    sponsors = mkdocs.utils.yaml_load(sponsors_data_path.read_text(encoding="utf-8"))
-    if not (match_start and match_end):
-        raise RuntimeError("Couldn't auto-generate sponsors section")
-    if not match_pre:
-        raise RuntimeError("Couldn't find pre section (<style>) in index.md")
-    frontmatter_end = match_pre.end()
-    pre_end = match_start.end()
-    post_start = match_end.start()
-    template = Template(index_sponsors_template)
-    message = template.render(sponsors=sponsors)
-    pre_content = content[frontmatter_end:pre_end]
-    post_content = content[post_start:]
-    new_content = pre_content + message + post_content
-    return new_content
-
-
-@app.command()
-def generate_readme() -> None:
-    """
-    Generate README.md content from main index.md
-    """
-    typer.echo("Generating README")
-    readme_path = Path("README.md")
-    new_content = generate_readme_content()
-    readme_path.write_text(new_content, encoding="utf-8")
-
-
-@app.command()
-def verify_readme() -> None:
-    """
-    Verify README.md content from main index.md
-    """
-    typer.echo("Verifying README")
-    readme_path = Path("README.md")
-    generated_content = generate_readme_content()
-    readme_content = readme_path.read_text("utf-8")
-    if generated_content != readme_content:
-        typer.secho(
-            "README.md outdated from the latest index.md", color=typer.colors.RED
-        )
-        raise typer.Abort()
-    typer.echo("Valid README ✅")
-
 
 @app.command()
 def build_all() -> None:
