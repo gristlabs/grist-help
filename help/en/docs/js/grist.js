7844f81d638aaa6b986583698488294f807c834a
Spoffy <contact@spoffy.net>
2024-08-21 15:38:25 +0100

    Fixes sections not expanding on jump

--- help/en/docs/js/grist.js
+++ help/en/docs/js/grist.js
@@ -6,13 +6,27 @@
 function expandSelected() {
   var hash = window.location.hash.split('/').slice(-1)[0];
   var elem = hash ? document.querySelector(hash) : null;
-  if (elem && elem.tagName === 'DETAILS') {
-    for (var el of document.querySelectorAll('details')) {
-      el.open = (el === elem);
+  var elemToTest = elem;
+  // Find an expandable parent node.
+  while (elemToTest) {
+    if (elemToTest && elemToTest.tagName === 'DETAILS') {
+      break;
     }
-    // After collapsing other details, the scroll position may be off, so fix it now.
-    elem.scrollIntoView();
+    elemToTest = elemToTest.parentElement;
   }
+
+  if (!elemToTest) {
+    return;
+  }
+
+  var elemToExpand = elemToTest;
+
+  for (var el of document.querySelectorAll('details')) {
+    el.open = (el === elemToExpand);
+  }
+
+  // After collapsing other details, the scroll position may be off, so fix it now.
+  elem.scrollIntoView();
 }
 
 // Function to get and auto play YouTube video from data tag.
