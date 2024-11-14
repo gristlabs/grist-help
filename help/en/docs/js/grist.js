gristlabs/grist-help

commit f58dbfa4d0bc4128918218ffaeab5284026ba6dd

Fixes function searching and jumping (#389)
- Reworks how function names are rendered on the functions page, meaning we don't need a hidden header element.
- Adds attr_list as an extension (I think it should always have been there?)
- Adds styling to make the (now visible) header elements match what was there before.
- Fixes sections not expanding when jumped to via permalink or searching

help/en/docs/js/grist.js
@@ -6,13 +6,18 @@
 function expandSelected() {
   var hash = window.location.hash.split('/').slice(-1)[0];
   var elem = hash ? document.querySelector(hash) : null;
-  if (elem && elem.tagName === 'DETAILS') {
-    for (var el of document.querySelectorAll('details')) {
-      el.open = (el === elem);
-    }
-    // After collapsing other details, the scroll position may be off, so fix it now.
-    elem.scrollIntoView();
+  var closestExpandableElem = elem.closest('details');
+
+  if (!closestExpandableElem) {
+    return;
   }
+
+  for (var el of document.querySelectorAll('details')) {
+    el.open = (el === closestExpandableElem);
+  }
+
+  // After collapsing other details, the scroll position may be off, so fix it now.
+  elem.scrollIntoView();
 }
 
 // Function to get and auto play YouTube video from data tag.

help/en/docs/css/grist.css
@@ -316,16 +316,16 @@ body {
 }
 
 /*
- * We want headers for the sake of searching, but don't want to see the huge padding and
- * surrounding empty paragraphs.
+ * Hide the empty paragraphs that are generated around headers in the expanding sections.
  */
-.wm-page-content summary h4,
-.wm-page-content summary p {
+.wm-page-content summary p:empty {
   display: none;
 }
-/* The <code> block that serves as a function header annoyingly gets auto-wrapped in a <p> */
-.wm-page-content summary p:not(:first-child) {
-  display: inline-block;
+
+/*
+ * Clean up any extra space used by paragraphs in the header.
+ */
+.wm-page-content summary p {
   margin: 0;
 }
 

mkdocs.yml
@@ -214,6 +214,7 @@ markdown_extensions:
   smarty: {}
   extra: {}
   meta: {}
+  attr_list: {}
 
 hooks:
 - ../../mkdocs-utils/hooks.py

mkpydocs.py
@@ -148,10 +148,8 @@ def get_doc_text(docitems):
     output.append('### ' + category)
     for d in items:
       css_class = 'class="unimplemented"' if d.is_unimplemented else ""
-      output.append('<details id="%s" markdown><summary %s>' % (d.anchor, css_class))
-      output.append('#### %s' % d.names[0])
-      output.append('<code>%s</code>' % (d.usage,))
-      output.append('<a class="headerlink" href="#%s" title="Permanent link">#</a>' % d.anchor)
+      output.append(f'<details markdown><summary {css_class}>')
+      output.append(f'#### <code>{d.usage}</code> {{: #{d.anchor} data-toc-label="{d.names[0]}" }}')
       output.append('</summary>')
       output.extend(d.format_doc())
       if d.is_unimplemented:

commit b4f8378dff02dc6e5171e18eea6c4843c97879c0

Fix silly JS error introduced in #389, which prevented some initialization.
Fixes #444

help/en/docs/js/grist.js
@@ -6,6 +6,7 @@
 function expandSelected() {
   var hash = window.location.hash.split('/').slice(-1)[0];
   var elem = hash ? document.querySelector(hash) : null;
+  if (!elem) { return; }
   var closestExpandableElem = elem.closest('details');
 
   if (!closestExpandableElem) {